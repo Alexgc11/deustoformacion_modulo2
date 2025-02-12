3a728b9258751b08cf942fadb838b51b88502d11
Alexgc11 <alexgc_11@hotmail.com>
2025-02-12 19:24:53 +0100

    add (punto 3): creando clase Alumno con propiedades privadas y constructor

---
+++ script.js
@@ -0,0 +1,12 @@
+// Definición de la clase Alumno
+class Alumno {
+  #nombre;
+  #apellidos;
+  #puntos;
+
+  constructor(nombre, apellidos, puntos) {
+    this.#nombre = nombre;
+    this.#apellidos = apellidos;
+    this.#puntos = puntos;
+  }
+}

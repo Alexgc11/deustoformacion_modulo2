697718cdee4fed5d39adf754b8e230723244f48c
Alexgc11 <alexgc_11@hotmail.com>
2025-02-12 20:07:00 +0100

    add (punto 5): creando método evaluarAlumno para devolver promesa con datos alumno

--- script.js
+++ script.js
@@ -30,3 +30,33 @@ class Alumno {
     }`;
   }
 }
+
+// Función que devuelve una promesa después de 2 segundos
+function evaluarAlumno() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let name = document.getElementById("name").value.trim();
+      let surname = document.getElementById("surname").value.trim();
+      let points = document.getElementById("points").value.trim();
+
+      if (!name || !surname || points === "") {
+        reject("Datos no validos");
+        return;
+      }
+
+      let puntos = Number(points);
+
+      if (isNaN(puntos) || puntos < 0 || puntos > 10) {
+        reject("La puntuación debe estar entre 0 y 10");
+        return;
+      }
+
+      try {
+        const alumno = new Alumno(name, surname, puntos);
+        resolve(alumno.getInfo());
+      } catch (error) {
+        reject(error.message);
+      }
+    }, 2000);
+  });
+}

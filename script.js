21c1d2dd441c4e53ec378d926d4e831051384793
Alexgc11 <alexgc_11@hotmail.com>
2025-02-12 19:31:35 +0100

    add (punto 4): creando método  con validación

--- script.js
+++ script.js
@@ -9,4 +9,13 @@ class Alumno {
     this.#apellidos = apellidos;
     this.#puntos = puntos;
   }
+
+  // Método SET para establecer puntos con validación
+  set puntos(valor) {
+    if (valor >= 0 && valor <= 10) {
+      this.#puntos = valor;
+    } else {
+      throw new Error("La puntuación debe estar entre o y 10.");
+    }
+  }
 }
